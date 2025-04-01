2f380bdb595e1244f8fb860c21aa007cdaa5deea
CoconutWater <95958608+CooledWater@users.noreply.github.com>
2025-04-01 11:34:58 +0800

    hello again from wang cheng

--- share-eclipse-project-on-github/src/HelloWorld.java
+++ share-eclipse-project-on-github/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world from Wang Cheng!");
-		System.out.println("Hello again");
+		System.out.println("Hello again from Wang Cheng!");
 		// comment from Wang Cheng
 		// new comment from Wang Cheng
 		
