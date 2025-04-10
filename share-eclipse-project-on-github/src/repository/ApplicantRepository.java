8f0f73c8b2c8c24d5e07f082d95e4f1dc8962cd7
Factor35 <philiphan35@gmail.com>
2025-04-11 00:05:40 +0800

    Updated applicantRepository to follow code from managerRepository

--- share-eclipse-project-on-github/src/repository/ApplicantRepository.java
+++ share-eclipse-project-on-github/src/repository/ApplicantRepository.java
@@ -1,5 +1,54 @@
-package services;
+package repository;
+import java.util.*;
+import java.io.*;
 
-public class ApplicantRepository {
+import entity.Applicant;
 
+public class ApplicantRepository extends RepositoryWithCSV {
+
+	private static final long serialVersionUID = -4139573877991643327L; // to be edited
+	private List<Applicant> applicants;
+	
+	public ApplicantRepository() {
+		this.applicants = new ArrayList<Applicant>();
+	}
+	
+	public void importFromCSV() {
+		String filePath = "csv\\ApplicantList.csv";
+		Scanner sc;
+		try {
+			sc = new Scanner(new FileReader(filePath));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("ManagerList.csv file is not found. Please ensure that the file is inside csv folder. ");
+			return;
+		}
+		
+		sc.nextLine(); // skip the csv list heading
+		while(sc.hasNext()) {
+			Applicant newApplicant = new Applicant();
+			String line = sc.nextLine();
+			String[] parts = line.split(",");
+			newApplicant.setName(parts[0]);
+			newApplicant.setNRIC(parts[1]);
+			newApplicant.setAge(Integer.parseInt(parts[2]));
+			newApplicant.setMarried(parts[3].equals("Married")? true : false);
+			newApplicant.setPassword(parts[4]);
+			this.applicants.add(newApplicant);
+			
+		}
+		
+		sc.close();
+	}
+	
+	
+	
+	public List<Applicant> getApplicants() {
+		return applicants;
+	}
+	
+	public Applicant searchByNRIC(applicantNRIC) {
+		// to be implemented
+		
+	}
 }
