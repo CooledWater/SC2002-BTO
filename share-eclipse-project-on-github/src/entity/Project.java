0dfc097525e9bbfa8a3255359764fb025998d0bc
CooledWater <95958608+CooledWater@users.noreply.github.com>
2025-04-11 20:52:24 +0800

    added a constructor for Project class

--- share-eclipse-project-on-github/src/entity/Project.java
+++ share-eclipse-project-on-github/src/entity/Project.java
@@ -3,31 +3,111 @@ package entity;
 import java.util.*;
 
 public class Project {
-    private boolean isVisible;
+    
+    // attributes defined in csv file
     private String name;
     private String neighbourhood;
-    private FlatType flatType;
+    private int numberOf2Rooms;
+    private int numberof3Rooms;
+    private int sellingPrice2Room;
+    private int sellingPrice3Room;
     private String openDate;
     private String closeDate;
-    private String managerName;
-    private List<Officer> officersAssigned = new ArrayList<>();
-    private List<ProjectApp> projectApps = new ArrayList<>();
-
-    public Project(String name, String neighbourhood, FlatType flatType, String openDate, String closeDate, String managerName) {
-        this.name = name;
-        this.neighbourhood = neighbourhood;
-        this.flatType = flatType;
-        this.openDate = openDate;
-        this.closeDate = closeDate;
-        this.managerName = managerName;
-        this.isVisible = true;
-    }
-
-    public void addOfficer(Officer officer) {
-        officersAssigned.add(officer);
-    }
+    private Manager manager;
+    private int numberOfOfficers;
+    private List<Officer> officers;
     
-    public String getName() {
-    	return name;
+    
+    // other attributes
+    private boolean isVisible;
+    private List<ProjectApp> projectApps;
+    
+    // this constructor can be used while importing from CSV
+    public Project(String name, String neighbourhood, int numberOf2Rooms,
+    		int sellingPrice2Room, int numberOf3Rooms, int sellingPriceOf3Room, 
+    		String openDate, String closeDate, Manager manager, int numberOfOfficers,
+    		List<Officer> officers) { 
+    	this.name = name;
+    	this.neighbourhood = neighbourhood;
+    	this.numberOf2Rooms = numberOf2Rooms;
+    	this.sellingPrice2Room = sellingPrice2Room;
+    	this.numberof3Rooms = numberOf3Rooms;
+    	this.sellingPrice3Room = sellingPriceOf3Room;
+    	this.openDate = openDate;
+    	this.closeDate = closeDate;
+    	this.manager = manager;
+    	this.numberOfOfficers = numberOfOfficers;
+    	this.officers = officers;
+    	
+    	this.isVisible = true;
+    	this.projectApps = new ArrayList<>();
     }
+	public boolean isVisible() {
+		return isVisible;
+	}
+	public void setVisible(boolean isVisible) {
+		this.isVisible = isVisible;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNeighbourhood() {
+		return neighbourhood;
+	}
+	public void setNeighbourhood(String neighbourhood) {
+		this.neighbourhood = neighbourhood;
+	}
+	public int getNumberOf2Rooms() {
+		return numberOf2Rooms;
+	}
+	public void setNumberOf2Rooms(int numberOf2Rooms) {
+		this.numberOf2Rooms = numberOf2Rooms;
+	}
+	public int getNumberof3Rooms() {
+		return numberof3Rooms;
+	}
+	public void setNumberof3Rooms(int numberof3Rooms) {
+		this.numberof3Rooms = numberof3Rooms;
+	}
+	public String getOpenDate() {
+		return openDate;
+	}
+	public void setOpenDate(String openDate) {
+		this.openDate = openDate;
+	}
+	public String getCloseDate() {
+		return closeDate;
+	}
+	public void setCloseDate(String closeDate) {
+		this.closeDate = closeDate;
+	}
+	public Manager getManager() {
+		return manager;
+	}
+	public void setManager(Manager manager) {
+		this.manager = manager;
+	}
+	public List<Officer> getOfficers() {
+		return officers;
+	}
+	public void setOfficers(List<Officer> officers) {
+		this.officers = officers;
+	}
+	public int getNumberOfOfficers() {
+		return numberOfOfficers;
+	}
+	public void setNumberOfOfficers(int numberOfOfficers) {
+		this.numberOfOfficers = numberOfOfficers;
+	}
+	public List<ProjectApp> getProjectApps() {
+		return projectApps;
+	}
+	public void setProjectApps(List<ProjectApp> projectApps) {
+		this.projectApps = projectApps;
+	}
+
+
 }
