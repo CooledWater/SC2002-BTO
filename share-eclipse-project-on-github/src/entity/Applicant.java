8e0e2bdb3157afa58178ba5c0573cbc171e54bc1
Factor35 <157774754+Factor35@users.noreply.github.com>
2025-04-11 21:17:57 +0800

    Update Applicant.java

--- share-eclipse-project-on-github/src/entity/Applicant.java
+++ share-eclipse-project-on-github/src/entity/Applicant.java
@@ -7,7 +7,7 @@ public class Applicant extends User{
 	 * 
 	 */
 	private static final long serialVersionUID = 2241078442498755535L;
-	private List<ProjectApp> projectApps;
+	private ProjectApp projectApp;
 	private List<Enquiry> enquiries;
 	
 	public Applicant() {
@@ -18,16 +18,15 @@ public class Applicant extends User{
 	
 	public Applicant(String name, String NRIC, int age, boolean isMarried, String password) {
 		super(name, NRIC, age, isMarried, password);
-		this.projectApps = new ArrayList<>();
 		this.enquiries = new ArrayList<>();
 	}
 
-	public List<ProjectApp> getProjectApps() {
-		return projectApps;
+	public ProjectApp getProjectApp() {
+		return projectApp;
 	}
 
-	public void setProjectApps(List<ProjectApp> projectApps) {
-		this.projectApps = projectApps;
+	public void setProjectApp(ProjectApp projectApp) {
+		this.projectApp = projectApp;
 	}
 
 	public List<Enquiry> getEnquiries() {
