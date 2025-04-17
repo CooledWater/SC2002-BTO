af41219d9262697bd30516b811fb2108c48fedee
CooledWater <95958608+CooledWater@users.noreply.github.com>
2025-04-17 20:50:16 +0800

    created ApplicantMainMenu

---
+++ share-eclipse-project-on-github/src/interfaces/ApplicantMainMenu.java
@@ -0,0 +1,19 @@
+package interfaces;
+import services.*;
+import entity.*;
+public class ApplicantMainMenu {
+	private Applicant currentSessionApplicant;
+	public ApplicantMainMenu(Applicant applicant) {
+		this.currentSessionApplicant = applicant;
+	};
+	// change password
+	
+	// login
+	
+	// 
+	public void applicantMenu() {
+	}
+	
+	public void projectAppMenu() {};
+
+}
