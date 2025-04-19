fa98bee086b6e105f89dac1cf9ae0a7b14a14ed6
CooledWater <95958608+CooledWater@users.noreply.github.com>
2025-04-19 09:48:04 +0800

    fixed

--- share-eclipse-project-on-github/src/test/RepositoryImportFromSerTest.java
+++ share-eclipse-project-on-github/src/test/RepositoryImportFromSerTest.java
@@ -1,15 +1,27 @@
 package test;
 
 import repository.ManagerRepository;
+import repository.OfficerRepository;
+import repository.ProjectRepository;
+
 import java.io.*;
+
+import entity.Manager;
+import entity.Project;
 
 public class RepositoryImportFromSerTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, IOException {
-		ManagerRepository managerRepository = new ManagerRepository();
-		managerRepository = (ManagerRepository) managerRepository.importFromSer();
-		System.out.println(managerRepository.getManagers().getFirst().getManagingProj().getName());
+		ManagerRepository managerRepo = new ManagerRepository();
+		ProjectRepository projectRepo = new ProjectRepository();
+		OfficerRepository officerRepo = new OfficerRepository();
+		managerRepo = (ManagerRepository) managerRepo.importFromSer();
+		officerRepo = (OfficerRepository) officerRepo.importFromSer();
+		projectRepo = (ProjectRepository) projectRepo.importFromSer();
+
+		Manager thisManager = managerRepo.getManagers().getFirst();
+		
+		System.out.format("Manager %s is managing project %s. ", thisManager.getName(), thisManager.getManagingProj().getName());
 
 	}
-
 }
